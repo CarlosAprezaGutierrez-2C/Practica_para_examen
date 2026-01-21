fa5ce99c3c9deee3770961794c6a7334b287ade5
Carlos Apreza <20253ds088@utez.edu.mx>
2026-01-20 19:47:18 -0600

    Update

--- src/BuenasPracticas.java
+++ src/BuenasPracticas.java
@@ -1,4 +1,33 @@
+
+import java.util.Scanner;
 public class BuenasPracticas {
+
+    public static int Numeros;
+    public static double SUMA=0.0;
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+     double CalcularSuma=CalcularSuma(sc);
+     double CalcularPromedio=CalcularPromedio(CalcularSuma);
+     System.out.println("La suma total es: "+CalcularSuma);
+     System.out.println("El promedio de la suma es :  "+CalcularPromedio);
+
 
 
 }
+
+public static double CalcularSuma(Scanner sc){
+    System.out.println("Ingresa cuantos numeros quieres calcular");
+     Numeros=sc.nextInt();
+     for(int i=1;i<=Numeros;i++){
+         System.out.println("Ingresa el numero "+i);
+     SUMA+=sc.nextDouble();
+     }
+    return SUMA;
+    }
+    public static double CalcularPromedio(Double SUMA){
+
+        return SUMA/Numeros;
+    }
+}
+
+
